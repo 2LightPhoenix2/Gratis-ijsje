2ebac308d56458bc960a3784a6020e67b10c77e6
2LightPhoenix2 <125906@chrlyceumdelft.nl>
2026-06-10 03:50:50 +0000

    Inleveren 3

---
+++ qr.js
@@ -0,0 +1,18 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qr-image");
+let qrText = document.getElementById("qr-text");
+
+function generateQR() {
+
+    if(qrText.value.length > 0){
+
+        qrImage.src =
+        "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+        + encodeURIComponent(qrText.value);
+
+        imgBox.classList.add("show-img");
+    }
+    else{
+        alert("Voer eerst tekst of een URL in!");
+    }
+}
